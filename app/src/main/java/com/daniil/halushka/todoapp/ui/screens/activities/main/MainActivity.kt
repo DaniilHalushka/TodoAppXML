97c5284ef277f3e76d7f295e371f1061be2771f3
Daniil Halushka <popolzen22@gmail.com>
2024-06-17 13:23:34 +0300

    Setup toolbar visibility in fragment, fix commit

--- app/src/main/java/com/daniil/halushka/todoapp/ui/screens/activities/main/MainActivity.kt
+++ app/src/main/java/com/daniil/halushka/todoapp/ui/screens/activities/main/MainActivity.kt
@@ -2,7 +2,11 @@ package com.daniil.halushka.todoapp.ui.screens.activities.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.daniil.halushka.todoapp.R
+import com.daniil.halushka.todoapp.constants.Constants
 import com.daniil.halushka.todoapp.databinding.ActivityMainBinding
+import com.daniil.halushka.todoapp.ui.screens.fragments.TodoListFragment
+import com.daniil.halushka.todoapp.util.replaceFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mainBinding: ActivityMainBinding
@@ -10,5 +14,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
+
+        Constants.APP_ACTIVITY = this
+        replaceFragment(TodoListFragment(), R.id.main_data_container, false)
+
     }
 }
--- app/src/main/java/com/daniil/halushka/todoapp/ui/screens/fragments/TodoListFragment.kt
+++ app/src/main/java/com/daniil/halushka/todoapp/ui/screens/fragments/TodoListFragment.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.daniil.halushka.todoapp.R
 import com.daniil.halushka.todoapp.data.repository.TodoRepository
 import com.daniil.halushka.todoapp.databinding.FragmentTodoListBinding
 import com.daniil.halushka.todoapp.ui.screens.adapters.TodoAdapter
 
-class TodoListFragment : Fragment() {
+class TodoListFragment : Fragment(R.layout.fragment_todo_list) {
     private lateinit var todoListBinding: FragmentTodoListBinding
+    private lateinit var todoListRecycler: RecyclerView
     private lateinit var todoListAdapter: TodoAdapter
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,12 +28,24 @@ class TodoListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        todoListBinding.todoListRecycler.layoutManager = LinearLayoutManager(requireContext())
+        val toolbar = todoListBinding.toolbar
+        toolbar.title = ""
+        (requireActivity() as AppCompatActivity).setSupportActionBar(toolbar)
+    }
 
+    override fun onResume() {
+        super.onResume()
+
+        initializeRecyclerView()
+    }
+
+    private fun initializeRecyclerView() {
         val repository = TodoRepository()
         val todoList = repository.getTodoList()
 
+        todoListRecycler = todoListBinding.todoListRecycler
         todoListAdapter = TodoAdapter(todoList)
+
         todoListBinding.todoListRecycler.adapter = todoListAdapter
     }
 }
